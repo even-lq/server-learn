6f063c1d7eb2f1d65ecb2fb7b48812e9965ab4d3
even-lq <lqnwd999@163.com>
2021-11-30 23:21:47 +0800

    mysql config

---
+++ blog-raw/src/conf/db.js
@@ -0,0 +1,28 @@
+const env = process.env.NODE_DEV; // 环境参数
+
+// 配置
+let MYSQL_CONF;
+
+if (env === 'dev') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'liqing',
+    port: '3306',
+    database: 'rawblog'
+  }
+}
+
+if (env === 'production') {
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'liqing',
+    port: '3306',
+    database: 'rawblog'
+  }
+}
+
+module.exports = {
+  MYSQL_CONF
+}
